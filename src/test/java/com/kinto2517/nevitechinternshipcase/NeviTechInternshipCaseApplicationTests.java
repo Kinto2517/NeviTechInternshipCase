15d31260d9ccc87f6c323ce9a2e2c0bcae132b85
Kinto2517 <ersin.yilmaz2517@gmail.com>
2023-11-13 23:44:08 +0300

    Initial commit

---
+++ src/test/java/com/kinto2517/nevitechinternshipcase/NeviTechInternshipCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinto2517.nevitechinternshipcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeviTechInternshipCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
