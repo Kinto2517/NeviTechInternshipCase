26b991d38cc58cb6eba10c280c813354932191f6
Kinto2517 <ersin.yilmaz2517@gmail.com>
2023-11-14 00:08:17 +0300

    Initializing Commit

---
+++ src/main/java/com/kinto2517/nevitechinternshipcase/NeviTechInternshipCaseApplication.java
@@ -0,0 +1,13 @@
+package com.kinto2517.nevitechinternshipcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeviTechInternshipCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeviTechInternshipCaseApplication.class, args);
+    }
+
+}
